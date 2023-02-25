8bed9022fcf3af0b5b22ab0b3c283f9a53eb1e34
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-02-25 00:48:11 -0300

    ??.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,109 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {Octokit} from '@octokit/action'
+import * as github from '@actions/github'
+
+import {cherryPickCommits} from 'github-cherry-pick'
+
+import semver from 'semver'
+
+/* 
+  Hotfix procedure:
+  gets the latest version of the action from the repo
+  calculates the next patch version
+  create a new branch from master if doesn't exist
+  cherry-pick the commit from merged pull request
+  push the branch to the repo
+  create a new draft pull request from the branch to master if doesn't exist
+*/
+
+const octokit = new Octokit()
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    // gets the latest version of the action from the repo
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const {owner, repo} = github.context.repo
+    const {pull_request} = github.context.payload
 
-    core.setOutput('time', new Date().toTimeString())
+    if (!pull_request) {
+      throw Error('Only pull_request events are supported.')
+    }
+
+    if (!pull_request.merged) {
+      throw Error('Only merged pull requests are supported.')
+    }
+
+    core.debug(`Getting latest release from ${owner}/${repo}`)
+    const {data: latestRelease} = await octokit.repos.getLatestRelease({
+      owner,
+      repo
+    })
+
+    core.debug(`Latest release: ${latestRelease.tag_name}`)
+
+    // calculates the next patch version
+
+    core.debug(`Calculating next patch version`)
+
+    const releaseVersion = semver.inc(latestRelease.tag_name, 'patch')
+
+    const releaseBranch = `release/${releaseVersion}`
+
+    core.debug(`Next patch version: ${releaseVersion}`)
+    core.debug(`Release branch: ${releaseBranch}`)
+
+    // create a new branch from master if doesn't exist
+
+    core.debug(`Checking if branch ${releaseBranch} exists`)
+
+    const branch = await octokit.git.getRef({
+      owner,
+      repo,
+      ref: `heads/${releaseBranch}`
+    })
+
+    if (!branch.data) {
+      core.debug(`Branch ${releaseBranch} doesn't exist`)
+      core.debug(`Creating branch ${releaseBranch} from the latest release`)
+      await octokit.git.createRef({
+        owner,
+        repo,
+        ref: `heads/${releaseBranch}`,
+        sha: latestRelease.target_commitish
+      })
+    } else {
+      core.debug(`Branch ${releaseBranch} exists`)
+    }
+
+    // cherry-pick the commit from merged pull request
+    core.debug(
+      `Cherry-picking pull_request ${pull_request.number} to ${releaseBranch}`
+    )
+
+    // merge_commit_sha
+
+    await cherryPickCommits({
+      commits: [pull_request.merge_commit_sha],
+      head: releaseBranch,
+      octokit,
+      owner,
+      repo
+    })
+
+    // create a new draft pull request from the branch to master if doesn't exist
+
+    core.debug(`Creating pull request from ${releaseBranch} to master`)
+
+    const pullRequest = await octokit.pulls.create({
+      owner,
+      repo,
+      title: `Release ${releaseVersion}`,
+      head: releaseBranch,
+      base: 'master',
+      draft: true
+    })
+
+    core.debug(`Pull request created: ${pullRequest.data.number}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
